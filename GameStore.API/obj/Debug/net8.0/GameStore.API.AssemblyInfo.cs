f084edfeded8bef4724911e60295884892fa3273
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2024-04-21 14:31:25 -0300

    What is a REST API?

--- GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
+++ GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670a5e768ea34e38204db5709b7ccb9d1643647b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb671d813239230398d3aa9d1773e946ee80952d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

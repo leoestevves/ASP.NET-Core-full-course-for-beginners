7de6045535a557ef34e505704865fc0d5df600a6
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2024-04-23 17:07:36 -0300

    Implement CRUD Endpoints (3)

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -35,9 +35,9 @@ app.MapGet("games", () => games);
 //(Read)  GET /games/1
 app.MapGet("games/{id}", (int id) => 
 {
-    var game = games.Find(game => game.Id == id);   
+    var game = games.Find(game => game.Id == id);    
 
-    return game is null ? Results.NotFound() : Results.Ok(game);
+    return game is null ? Results.NotFound() : Results.Ok(game); //if game == null return results.NotFound(); else Results.Ok(game)
 })
 .WithName(GET_GAME_ENDPOINT_NAME); //Dando um nome
 
@@ -63,6 +63,11 @@ app.MapPost("games", (CreateGameDto newGame) => {
 app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => 
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
 
     games[index] = new GameDto
     (

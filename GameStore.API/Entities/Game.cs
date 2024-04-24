ba629cd26b23f2a907d8601d9c8af382167f8550
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2024-04-24 16:58:06 -0300

    Defining the data model

---
+++ GameStore.API/Entities/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace GameStore.API.Entities;
+
+public class Game
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public int GenreId { get; set; }
+
+    public Genre? Genre { get; set; } //O ? mostra que aceita receber nulo
+
+    public decimal Price { get; set; }
+
+    public DateOnly ReleaseDate { get; set; }
+}
--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,6 +1,12 @@
+using GameStore.API.Data;
 using GameStore.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Source=GameStore.db"; //Local que vai ser criado o db
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();

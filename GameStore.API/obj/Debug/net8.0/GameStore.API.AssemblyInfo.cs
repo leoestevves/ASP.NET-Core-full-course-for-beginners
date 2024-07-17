ba26d2e8158bca7b99a3cab4b37e08f93da8ed4c
leoestevves <leogoncalves60@gmail.com>
2024-07-17 12:48:15 -0300

    Update readme

--- GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
+++ GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b0c6207186a56d183b111f328ef3bd6e2adc85b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a03e9e2ad6447e13cb03c17bbb2366c03363358")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ASP.NET-Core-full-course-for-beginners
 Little project from [Julio Casal](https://www.youtube.com/watch?v=AhAxLiGC7Pc&ab_channel=JulioCasal) youtube channel, this is the backend part from the project. This project is for practice only.
+
+Link from frontend repository: https://github.com/leoestevves/Blazor-full-course-for-beginners
